df7605bd0233dd2292f74be9666ced073f0a95d7
Diego <adid1297@gmail.com>
2019-07-21 19:06:29 +0800

    Improvements to member form

---
+++ src/components/.common/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const CardTitle = ({ title }) => (
+  <div className="card-title">
+    <h6 className="title-text">{title}</h6>
+    <hr className="title-line" />
+  </div>
+);
--- src/components/MemberForm/MemberForm.jsx
+++ src/components/MemberForm/MemberForm.jsx
@@ -2,28 +2,14 @@ import React, { useState } from "react";
 
 import "../../styles/member-form.css";
 
-// const ExpandedMemberForm = () => (
-//   <div className="member-form card">
-//     <div className="member-form-row">
-//       <input type="text" className="form-input fill" placeholder="Member Name" />
-//     </div>
-//     <div className="member-form-row">
-//       <input type="number" className="form-input" placeholder="Number" />
-//       <input type="email" className="form-input" placeholder="Email Address" />
-
-//       <button className="form-input">
-//         Go
-//       </button>
-//     </div>
-//   </div>
-// );
-
 const MemberForm = ({ createMember }) => {
   const [newMemberName, setNewMemberName] = useState('');
   const handleChange = event => setNewMemberName(event.target.value);
   const handleSubmit = () => {
-    createMember(newMemberName);
-    setNewMemberName('');
+    if (newMemberName) {
+      createMember(newMemberName);
+      setNewMemberName('');
+    }
   }
   const handleKeyPress = ({ key }) => key === "Enter" && handleSubmit();
 
@@ -40,8 +26,13 @@ const MemberForm = ({ createMember }) => {
             onKeyPress={handleKeyPress}
           />
         </div>
-        <button className="form-input" onClick={handleSubmit}>
-          Go
+        <button
+          className={`member-form-submit ${newMemberName ? 'can-submit' : ''}`}
+          onClick={handleSubmit}
+        >
+          <i className="material-icons">
+            person_add
+          </i>
         </button>
       </div>
     </div>
