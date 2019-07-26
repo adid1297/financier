b93e1578576546320056b3401ef539b74aac44b3
Diego <adid1297@gmail.com>
2019-07-26 19:12:18 +0800

    Updates

---
+++ precache-manifest.3bd23f54dc627a604cd75c0396217f74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40e16eafb74b9a7a7d973cd7ed992eb",
+    "url": "/financier/index.html"
+  },
+  {
+    "revision": "a3b049743b73bdaf4800",
+    "url": "/financier/static/css/main.165aed49.chunk.css"
+  },
+  {
+    "revision": "eb17f968dc9645988765",
+    "url": "/financier/static/js/2.12e083db.chunk.js"
+  },
+  {
+    "revision": "a3b049743b73bdaf4800",
+    "url": "/financier/static/js/main.b0a0e196.chunk.js"
+  },
+  {
+    "revision": "7fb2e826e925931fd02e",
+    "url": "/financier/static/js/runtime~main.9cc0035a.js"
+  }
+]);
--- src/components/.common/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const CardTitle = ({ title }) => (
-  <div className="card-title">
-    <h6 className="title-text">{title}</h6>
-    <hr className="title-line" />
-  </div>
-);
